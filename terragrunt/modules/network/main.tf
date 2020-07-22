33f51371973a00ba5690d4e9ae1f2844729480c4
Joatin Granlund <granlundjoatin@gmail.com>
2020-07-22 02:13:57 +0200

    removed unsuported argument

--- terragrunt/modules/network/main.tf
+++ terragrunt/modules/network/main.tf
@@ -28,5 +28,4 @@ resource "google_service_networking_connection" "private_vpc_connection" {
   network                 = google_compute_network.vpc_network.id
   service                 = "servicenetworking.googleapis.com"
   reserved_peering_ranges = [google_compute_global_address.private_ip_address.name]
-  project                  = var.project_id
 }
